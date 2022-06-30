87767c1dd485d0b0b97779633d063d9bbc648519
ClauRozasG <89096127+ClauRozasG@users.noreply.github.com>
2022-06-29 22:10:22 -0500

    Notificaciones

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,17 @@
 module.exports = {
+  name: 'default',
   type: 'mysql',
-  url: 'mysql//root:admin@localhost:3306/effective_routes_ddd',
+  url: 'mysql://root:root@localhost:3306/notificaciones',
+  synchronize: true,
   migrationsRun: true,
   logging: true,
   timezone: '+0',
   bigNumberStrings: false,
-  entities: [process.env.ENVIRONMENT == 'prod' ? '**/infrastructure/persistence/typeorm/entities/*.js' : 'dist/**/infrastructure/persistence/typeorm/entities/*.js'],
-  migrations: [process.env.ENVIRONMENT == 'prod' ? 'common/infrastructure/persistence/typeorm/migrations/*.js' : 'dist/common/infrastructure/persistence/typeorm/migrations/*.js'],
+  entities: ['dist/domain-bbom/entities/*.js'],
+  migrationsTableName: 'migrations',
+  migrations: ['dist/migrations/*.js'],
   cli: {
-    migrationsDir: process.env.ENVIRONMENT == 'prod' ? 'common/infrastructure/persistence/typeorm/migrations' : 'src/common/infrastructure/persistence/typeorm/migrations',
+    migrationsDir: 'src/migrations',
   },
+  dropSchema: false,
 };
